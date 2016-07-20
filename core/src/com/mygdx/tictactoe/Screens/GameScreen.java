e0e680da31bdd0c63243982071cb9363659e8056
tamburyniarz <tamburyniarz@gmail.com>
2016-07-20 23:17:36 +0200

    Added playButtons on gameScreen

--- core/src/com/mygdx/tictactoe/Screens/GameScreen.java
+++ core/src/com/mygdx/tictactoe/Screens/GameScreen.java
@@ -5,15 +5,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.Array;
 import com.mygdx.tictactoe.Core.AbstractScreen;
 import com.mygdx.tictactoe.TicTacToe;
+import com.mygdx.tictactoe.util.IClickCallback;
+import com.mygdx.tictactoe.util.PlayButton;
 
 /**
  * Created by Senpai on 20.07.2016.
  */
 public class GameScreen extends AbstractScreen {
 
-    Texture bgTexture;
+    private Texture bgTexture;
+    private Array<PlayButton> playButtonArray;
 
     public GameScreen() {
         initTexture();
@@ -28,6 +32,34 @@ public class GameScreen extends AbstractScreen {
         Image bgbImage = new Image(bgTexture);
         bgbImage.setPosition(0, TicTacToe.HEiGHT / 2 - bgbImage.getWidth() / 2);
         addActor(bgbImage);
+
+        initPlayButtons();
+    }
+
+    private void initPlayButtons() {
+        final int SIZE = 110;
+        // Start positions
+        final int START_X = 30;
+        final int START_Y = 170;
+        final int INTERVAL = SIZE + 20;
+
+        playButtonArray = new Array<PlayButton>();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                PlayButton newPlayButton = new PlayButton(new IClickCallback() {
+                    @Override
+                    public void onClick() {
+
+                    }
+                });
+                newPlayButton.setSize(SIZE, SIZE);
+                newPlayButton.setPosition(START_X + INTERVAL * i, START_Y + INTERVAL * j);
+                newPlayButton.setDebug(true);
+                playButtonArray.add(newPlayButton);
+                addActor(newPlayButton);
+            }
+        }
     }
 
 
